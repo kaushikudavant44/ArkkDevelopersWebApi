cc1fb8c1928a584f84c48aac22c38af0403e2cf7
Jarves <Jarves@DESKTOP-TLMVIJ2>
2018-06-20 13:14:19 +0530

    ArkkDevelopersWebApi - #1

---
+++ ArkkDevelopersWebApi/src/test/java/com/bionische/arkkdevelopers/ArkkDevelopersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bionische.arkkdevelopers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArkkDevelopersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
